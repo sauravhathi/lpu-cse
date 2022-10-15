19a9f27b21f5a8572dfaa118f3b8b4fd13d6a5cc
sauravhathi <61316762+sauravhathi@users.noreply.github.com>
2022-10-16 00:56:25 +0530

    backbtn issue fixed

--- src/components/BackBtn.js
+++ src/components/BackBtn.js
@@ -17,7 +17,7 @@ const BackBtn = () => {
             } */}
 
             {
-                window.location.href === "https://sauravhathi.github.io/lpu-cse/#/" || window.location.href === "https://sauravhathi.github.io/lpu-cse" || window.location.href === "https://sauravhathi.github.io/lpu-cse/#/" || window.location.href === "https://sauravhathi.github.io/lpu-cse/#" ? "" :
+                window.location.href === "https://sauravhathi.github.io/lpu-cse/" || window.location.href === "https://sauravhathi.github.io/lpu-cse/#/" || window.location.href === "https://sauravhathi.github.io/lpu-cse" || window.location.href === "https://sauravhathi.github.io/lpu-cse/#/" || window.location.href === "https://sauravhathi.github.io/lpu-cse/#" ? "" :
                     <div className="grid grid-cols-1 justify-items-start">
                         <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                             Back</button>
